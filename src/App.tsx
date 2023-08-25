c327ff1f47e0558d12bcd85ac10698a88e356bba
Maria232627 <maria.lscarvalho01@gmail.com>
2023-08-24 21:36:20 -0300

    tentativa de router_dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import { ComponentFooter, ComponentHeader } from "./components";
-import { PageHome, PageList } from "./pages";
-import { GlobalStyle, Main } from "./styles/GlobalStyle";
-import { Dispatch, SetStateAction, useState } from "react";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { RouteSoftware } from "./routes"
+import { BrowserRouter } from "react-router-dom"
 
-export interface IPage {
-  setPage: Dispatch<SetStateAction<number>>
-}
 export function App() {
-  const [page, setPage] = useState(1)
-  let component
-  switch (page) {
-    case 1:
-      component = <PageHome />
-      break;
-    case 2:
-      component = <PageList />
-      break;
-    default:
-      component = <PageHome />
-      break;
-  }
   return (
     <>
-      <GlobalStyle />
-      <ComponentHeader setPage={setPage} />
-      <Main>
-        {component}
-      </Main>
-      <ComponentFooter />
+      <BrowserRouter>
+        <GlobalStyle />
+        <RouteSoftware />
+      </BrowserRouter>
     </>
   )
 }
